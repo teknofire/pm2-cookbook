723c57c321721d93ab80e4b300fdf226ddeff32c
Will Fisher <will@gina.alaska.edu>
2017-01-20 11:17:59 -0900

    bump version to 0.2.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'will@teknofire.net'
 license 'Apache 2.0'
 description 'Installs/Configures node_pm2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '0.2.1'
 
 source_url 'https://github.com/teknofire/pm2-cookbook'
 issues_url 'https://github.com/teknofire/pm2-cookbook/issues'
